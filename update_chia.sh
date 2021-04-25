d41544a7491c08532acb32a6883b10dc338afd4d
Tung Vu <tv.stock@gmail.com>
2021-04-25 20:00:05 +0200

    init commit

---
+++ update_chia.sh
@@ -0,0 +1,10 @@
+cd $HOME/git/chia-blockchain/
+git checkout main
+git pull
+git reset --hard
+git checkout 1.1.2
+sh install.sh
+. ./activate
+chia stop all
+sudo pkill -9 chia_daemon
+chia start harvester
